53b88b5f7abaa795374a43be0036f3506ae3bcd4
AndromedaOMA <116078879+AndromedaOMA@users.noreply.github.com>
2023-04-23 19:20:22 +0300

    am adaugat modificari la laboratorul 8

--- Lab8/Pb1/main.cpp
+++ Lab8/Pb1/main.cpp
@@ -63,16 +63,16 @@ int main()
 	//{
 
 	//}
-
+/*
 	std::vector<std::pair<std::string, int>> vec(MAP.begin(), MAP.end());
 	std::sort(vec.begin(), vec.end(), [](const auto& a, const auto& b) { return a.second > b.second; });
-	/*
-	std::priority_queue<
-		T,               // tipul elementelor din coada
-		Container,       // containerul subiacent utilizat pentru stocare
-		Compare          // functia comparator pentru sortare (optional)
-	> pq;
-	*/
+	
+	///std::priority_queue<
+	///		T,               // tipul elementelor din coada
+	///		Container,       // containerul subiacent utilizat pentru stocare
+	///		Compare          // functia comparator pentru sortare (optional)
+	///> pq;
+	
 	std::priority_queue<std::pair<std::string, int>, std::vector<std::pair<std::string, int>>, std::greater<>> pq(vec.begin(), vec.end());
 	while (!pq.empty()) {
 		std::pair<std::string, int> top_element = pq.top();
@@ -82,6 +82,41 @@ int main()
 		pq.pop();
 	}
 	std::cout << '\n';
+	*/
+
+
+
+	struct
+	{
+		bool operator() (const std::pair<std::string, int> l, const std::pair<std::string, int> r) const {
+			if (l.second == r.second)
+			{
+				return l.first > r.first;
+			}
+
+			return l.second < r.second;
+		}
+	} comparator;
+
+
+	std::priority_queue<std::pair<std::string, int>, std::vector<std::pair<std::string, int>>, decltype(comparator)> pq{};
+	for (auto& it : MAP)
+	{
+		pq.push(it);
+	}
+
+
+	while (!pq.empty()) {
+		auto& top_element = pq.top();
+		auto& key = top_element.first;
+		int val = top_element.second;
+		std::cout << key << ':' << val << std::endl;
+		pq.pop();
+	}
+
+	std::cout << '\n';
+
+
 
 	return 0;
 }
